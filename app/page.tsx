bded45a3b4416d814aef6d92f5ea48cd2d935249
sandiarbaa <sandiarbaa@gmail.com>
2024-07-27 09:34:56 +0700

    fetching data dari public api fakestoreapi.com

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,23 @@
+"use client";
+import axios from "axios";
+import { useEffect } from "react";
+
 export default function Home() {
+  const fetchProducts = async () => {
+    try {
+      const productsResponse = await axios.get(
+        "https://fakestoreapi.com/products?limit=5"
+      );
+      console.log(productsResponse.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <main>
       <h1>Hello</h1>
