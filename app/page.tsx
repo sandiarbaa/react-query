40f037e6e9b1a13adab7c69afcf3baa0aae136c7
sandiarbaa <sandiarbaa@gmail.com>
2024-07-27 10:03:30 +0700

    fetching data products lalu render di table

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,80 @@
 "use client";
-import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { axiosInstance } from "./lib/axios";
+import Image from "next/image";
 
 export default function Home() {
-  // Function untuk fetch dataProducts (Read)
+  const [products, setProducts] = useState<[]>([]);
+
+  // Function untuk fetch products
   const fetchProducts = async () => {
     try {
       const productsResponse = await axiosInstance.get("/products?limit=5");
-      console.log(productsResponse.data);
+      setProducts(productsResponse.data);
     } catch (error) {
       console.log(error);
     }
   };
 
-  // Misalnya
-  // Function untuk create dataProducts (Create)
-  const createProducts = async () => {
-    try {
-      const productsResponse = await axiosInstance.post("/products"); // jika endpoint nya sama, cukup panggil url setelahnya saja kemana, dan ganti method nya
-      console.log(productsResponse.data);
-    } catch (error) {
-      console.log(error);
+  // Function untuk merender products
+  const renderProducts = () => {
+    interface ProductProps {
+      id: number;
+      title: string;
+      price: string;
+      category: string;
+      description: string;
+      image: string;
     }
+
+    return products.map((product: ProductProps) => {
+      return (
+        <tr key={product.id} className="border text-center">
+          <td className="border-[1.5px] p-2">{product.id}</td>
+          <td className="border-[1.5px] px-3">
+            {product.title.substring(0, 20)}
+          </td>
+          <td className="border-[1.5px] px-3">{product.price}</td>
+          <td className="border-[1.5px] px-3">{product.category}</td>
+          <td className="border-[1.5px]">
+            {product.description.substring(0, 20)}
+          </td>
+          <td className="border-[1.5px] px-3">
+            <Image
+              src={product.image}
+              alt="image"
+              width={100}
+              height={100}
+              loading="eager"
+              priority
+            />
+          </td>
+        </tr>
+      );
+    });
   };
 
+  // Fetch products saat halaman di render/refresh
   useEffect(() => {
     fetchProducts();
   }, []);
 
   return (
-    <main>
-      <h1>Hello</h1>
+    <main className="p-5">
+      <h1>Hello World!</h1>
+      <table className="table-auto">
+        <thead>
+          <tr className="border-[1.5px] bg-gray-500 text-white">
+            <th className="border-[1.5px] w-[40px]">No.</th>
+            <th className="border-[1.5px] w-[150px]">Title</th>
+            <th className="border-[1.5px] w-[100px]">Price</th>
+            <th className="border-[1.5px] w-[100px]">Category</th>
+            <th className="border-[1.5px] w-[100px]">Description</th>
+            <th className="border-[1.5px] w-[100px]">Image</th>
+          </tr>
+        </thead>
+        <tbody>{renderProducts()}</tbody>
+      </table>
     </main>
   );
 }
